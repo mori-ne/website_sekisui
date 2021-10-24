19315f28c6605f553465dd9191af54a5d9b64021
morine <fold.it.fold.it@gmail.com>
2021-10-24 21:29:37 +0900

    add slider

--- js/common.js
+++ js/common.js
@@ -1,5 +1,12 @@
 $('.mv').slick({
-    autoplay:false,
-    arrows:false,
-    dots: true
-  });
+  autoplay: false,
+  arrows: false,
+  dots: true
+});
+
+$('.pickup_slider').slick({
+  autoplay: true,
+  dots: false,
+  arrows: true,
+  slidesToShow: 4
+});
